8de7ac875893ed4c1424cdf2d59a1ec8c771893b
Igor Degtyarenko <div50015@mail.ru>
2024-02-13 15:38:10 +0300

    Edit test

--- tests/test_apparel-shoes.py
+++ tests/test_apparel-shoes.py
@@ -21,4 +21,4 @@ def test_page_apparel_shoes():
         browser.open(WEB_URL)
         browser.all('a[href="/apparel-shoes"]').first.click()
         # browser.all('//div[@class="header-menu"]/ul/li/a[@href="/books"]').first.click()
-#        time.sleep(5)
+        time.sleep(1)
